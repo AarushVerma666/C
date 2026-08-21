e57a69587507ac1fbb179a13770bd684fa990112
Aarush <aarush11th@gmail.com>
2026-08-21 16:45:55 +0530

    Initial commit

---
+++ HelloWorld.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main( )
+{
+    clrscr();
+    printf("My name is aarush verma\n");
+    printf("My branch is cs-ai");
+    return 0;
+}
+
---
+++ basic_calculator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    clrscr();
+    int r;
+    float area;
+    
+    
+}
