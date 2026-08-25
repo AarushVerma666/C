719111a3e875df96e9b767c252b11664799e2849
Aarush <aarush11th@gmail.com>
2026-08-25 22:08:15 +0530

    area of circle with user input

---
+++ ques2a.c
@@ -0,0 +1,11 @@
+// calculate area of circle
+
+#include <stdio.h>
+int main(){
+    float radius, area;
+    printf("Enter the radius of cricle: ");
+    scanf("%f",&radius);
+    area = 3.14*radius*radius;
+    printf("Area of circle is: %f",area);
+    return 0;
+}
